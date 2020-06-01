557320dcd21390fbee09714a3fd55d098ceec3cd
Ajay Singh Rana <ajaysinghrana494@gmail.com>
2020-06-01 20:52:04 +0530

    Initialization
    initialized the project by adding some initial files and text.

---
+++ main.py
@@ -0,0 +1,29 @@
+# k@mlesh
+# h3avren
+
+# So here we will start with the basics i.e. tokens,keywords and data types
+
+# print() is a function used in python to output (at the console) something. 
+
+# Tokens : The smallest unit used im python is token. These can ...
+
+""" Keywords : These are the reserved terms for any laguage and cannot be used by the user as a variable.Examples of keywords in
+    Python are for,if,else,elif,class,def,in,or,and,while,not,True,False..."""
+
+"""Data Types : These are categories that define what kind of some data is.For example 12 is a numerical data and "Rakesh" is a
+    textual data.So both "Rakesh" and 12 can not tereated as one.Therefore languages define some categories to define this data called 
+    Data Types.These are string (str),integer (int),floating point (float) and many more ."""
+
+# integer data :
+integer = 18
+print(f"{integer} is of the type {type(integer)}")  # type() is a function that returns or tells the data type of any object 
+""" here a special type of string substitution is used...it is called 'f string' ...in this we put an 'f' before any string then we 
+    directly type the name of the variable name between '{}'"""
+string = "The only thing that is constant is 'change'."
+print(f"'{string}' is of the type {type(string)}")
+float_value = 34.56
+print(f"{float_value} is of the type {type(float_value)}")
+
+# operators like '+','-','x' are used to conduct calculations .These are also provided by programming languages
+
+# The plus ('+') operator ::=>
